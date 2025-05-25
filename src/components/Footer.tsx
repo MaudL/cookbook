a9be1d4aa6d6ec5cc39497d5024307a38fea2c80
MaudL <34247091+MaudL@users.noreply.github.com>
2025-05-25 15:30:07 +0200

    switch owner

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,17 +3,6 @@ import React from 'react'
 export default function Footer() {
   return (
     <div className="mb-8 text-center">
-      <p className="text-">
-        Fait avec amour par{' '}
-        <a
-          className="text-primary-700 hover:underline"
-          href="https://github.com/JulienUsson/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Julien Usson
-        </a>
-      </p>
       <a className="text-gray-300 hover:underline" href="/admin/" color="textSecondary">
         Admin
       </a>
